e1114191d27f4a6d16c1db5771d64574e41e55f5
javachen <junezchen@gmail.com>
2019-07-13 14:30:55 +0800

    修改user-serice

--- cshop/cshop-user/cshop-user-api/src/main/java/com/javachen/api/UserApi.java
+++ cshop/cshop-user/cshop-user-api/src/main/java/com/javachen/api/UserApi.java
@@ -7,11 +7,8 @@ import com.javachen.model.form.PasswordReset;
 import com.javachen.model.form.UserLogin;
 import com.javachen.model.form.UserRegister;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.validation.Valid;
 
 public interface UserApi {
     @PostMapping(value = "/login")
