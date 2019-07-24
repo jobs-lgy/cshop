c113b65e1b89095b597f6a7ef4de9061ce29147c
javachen <junezchen@gmail.com>
2019-07-25 02:11:03 +0800

    集成nacos

--- cshop-basic/cshop-sms/src/main/java/com/javachen/cshop/sms/AppSms.java
+++ cshop-basic/cshop-sms/src/main/java/com/javachen/cshop/sms/AppSms.java
@@ -3,9 +3,10 @@ package com.javachen.cshop.sms;
 import com.javachen.cshop.sms.processor.SmsInputProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableBinding(value = {SmsInputProcessor.class})
 @SpringBootApplication
 public class AppSms {
